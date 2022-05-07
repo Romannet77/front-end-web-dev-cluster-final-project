e3f29582ba6fa4855f59240d5d08efa90b9db8ed
Roman Yurchenko <yurchenkor33@gmail.com>
2022-05-07 09:56:31 +0300

    adding english language to the map

--- js/script.js
+++ js/script.js
@@ -149,10 +149,14 @@ buttonSubmitCity.addEventListener("click", function (e) {
 
 let map = L.map("map").setView([48.92, 24.71], 13);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+var OpenStreetMap_DE = L.tileLayer(
+  "https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png",
+  {
+    maxZoom: 18,
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }
+).addTo(map);
 
 L.marker([48.92, 24.71])
   .addTo(map)
